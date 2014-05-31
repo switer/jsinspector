2b61d29a132e40ed3e1c49521f1cf10e13638e4c
switer <guankaishe@gmail.com>
2014-05-31 16:51:41 +0800

    '/' redirect to '/devtools'

--- server.js
+++ server.js
@@ -173,6 +173,10 @@ app.post('/html/delta', inspectorIdParse, rawBody, function (req, res) {
 /**
  *  Inspctor routes
  **/
+app.get('/', function (req, res, next) {
+    var search = url.parse(req.url).search;
+    res.redirect('/devtools' + (search || ''));
+});
 app.get('/devtools', inspectorIdParse, function (req, res, next) {
     if (!req.inspectorId) {
         res.send(200, 'Create a inspector id ?');
