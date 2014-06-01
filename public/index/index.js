3b6b5f339273fb3da30587fbc633b6a8376a87d7
switer <guankaishe@gmail.com>
2014-06-01 14:01:00 +0800

    styles

--- public/index/index.js
+++ public/index/index.js
@@ -2,25 +2,49 @@
     /* =================================================================== */
 
     function UUID() {
-        return 'xxxxxx-yxxxyx'.replace(/[xy]/g, function(c) {
+        var uuid = localStorage.getItem('__jsinspector_uuid__');
+        if (uuid) return uuid;
+        else {
+            uuid = UUID.generate();
+            localStorage.setItem('__jsinspector_uuid__', uuid);
+            return uuid;
+        }
+    }
+
+    UUID.generate = function () {
+        return 'xxxx-yxxyx'.replace(/[xy]/g, function(c) {
             var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
         }).toUpperCase();
     }
 
-    var script = '<script src="{{host}}/inspector?{{inspectId}}"></script>',
-        scriptContainer = document.querySelector('#script'),
-        code = script.replace('{{host}}', window.location.origin)
-                    .replace('{{inspectId}}', UUID())
-                    .replace(/</g, '&lt;')
-                    .replace(/>/g, '&gt;')
-                    .replace(/"([^"]*?)"/g, '<span class="string">"$1"</span>')
-                    .replace(/&lt;/g, '<span class="tag">&lt;</span>')
-                    .replace(/&gt;/g, '<span class="tag">&gt;</span>')
+    function createScript () {
+        var script = '<script src="{{host}}/inspector?{{inspectId}}"></script>',
+            $scriptContainer = document.querySelector('#script'),
+            code = script.replace('{{host}}', window.location.origin)
+                        .replace('{{inspectId}}', UUID())
+                        .replace(/</g, '&lt;')
+                        .replace(/>/g, '&gt;')
+                        .replace(/"/g, '&quot;')
+                        .replace(/\s+([a-zA-Z\-]+?)\=&quot;/g, ' <span class="attr"> $1</span>=&quot;')
+                        .replace(/=&quot;(.+?)&quot;/g, '=<span class="string">&quot;$1&quot;</span>')
+                        .replace(/&lt;([a-zA-Z]+?)\s/g, '&lt;<span class="tagname">$1 </span>')
+                        .replace(/\/([a-zA-Z]+?)&gt;/g, '<span class="tagclose">/</span><span class="tagname">$1</span>&gt;')
+                        .replace(/&lt;/g, '<span class="tag">&lt;</span>')
+                        .replace(/&gt;/g, '<span class="tag">&gt;</span>')
 
-    scriptContainer.innerHTML = code;
-
-    window.gotoInspector = function () {
-        window.open(window.location.origin + '/devtools?' + UUID());
+        $scriptContainer.innerHTML = code;
     }
+
+    createScript();
+    
+
+    var $devtoolsButton = document.querySelector('#devtoolsButton');
+    $devtoolsButton.href = window.location.origin + '/devtools?' + UUID();
+
+    document.querySelector('#createButton').onclick = function () {
+        localStorage.removeItem('__jsinspector_uuid__')
+        createScript();
+    }
+
 })();
