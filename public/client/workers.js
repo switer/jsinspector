4c70749fe5438544e1f1a8e06485dde25d47c9a5
switer <guankaishe@gmail.com>
2014-06-02 21:14:29 +0800

    try to use webworker

---
+++ public/client/workers.js
@@ -0,0 +1,110 @@
+function type (obj) {
+    return Object.prototype.toString.call(obj).match(/\[object (\w+)\]/)[1].toLowerCase();
+}
+this.addEventListener('message', function (event) {
+    if (type(event.data) !== 'object') return;
+
+    var options = event.data;
+
+    if (options.static) {
+        request({
+            url: '/src',
+            data: {
+                url: encodeURIComponent(options.url)
+            },
+            success: function (data, xhr) {
+                postMessage({
+                    formWorker: true,
+                    id: options.id,
+                    data: data,
+                    xhr: {
+                        statusText: xhr.statusText, 
+                        status: xhr.status,
+                        response: xhr.response,
+                        responseText: xhr.responseText, 
+                        statusText: xhr.statusText
+                    }
+                });
+            }
+        });
+    } else {
+        request({
+            method: options.method,
+            url: options.url,
+            data: options.data,
+            type: options.type,
+            success: function (data, xhr) {
+                postMessage({
+                    formWorker: true,
+                    id: options.id,
+                    data: data,
+                    xhr: {
+                        statusText: xhr.statusText, 
+                        status: xhr.status,
+                        response: xhr.response,
+                        responseText: xhr.responseText, 
+                        statusText: xhr.statusText
+                    }
+                });
+            },
+            error: function (err, xhr) {
+                postMessage({
+                    formWorker: true,
+                    id: options.id,
+                    error: err || xhr.response,
+                    xhr: {
+                        statusText: xhr.statusText, 
+                        status: xhr.status,
+                        response: xhr.response,
+                        responseText: xhr.responseText, 
+                        statusText: xhr.statusText
+                    }
+                });
+            }
+        });                
+    }
+});
+
+/**
+ *  ajax request
+ **/
+function request (options) {
+    var xhr = new XMLHttpRequest(),
+        method = options.method ? options.method.toUpperCase() : "GET";
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var data = xhr.responseText;
+            options.success && options.success(data, xhr);
+        } else if (xhr.readyState == 4 && xhr.status != 200) {
+            options.error && options.error('', xhr);
+        } 
+    }
+
+    var params = [];
+    if (options.data && Object.prototype.toString.call(options.data) !== '[object String]') {
+        // to query string
+        var keys = Object.keys(options.data);
+        keys.forEach(function (item, index) {
+            var value = options.data[item];
+            if (Object.prototype.toString.call(value) == '[object String]') {
+                params.push(item + '=' + value);
+            } else {
+                params.push(item + '=' + JSON.stringify(value));
+            }
+        });
+        params = params.join('&');
+    } else {
+        params = options.data;
+    }
+
+    if (method == 'POST' || method == 'PUT' || !options.data) {
+        xhr.open(method, options.url, false);
+        xhr.setRequestHeader("Content-Type", options.type || "application/x-www-form-urlencoded");
+        xhr.send(params);
+        
+    } else {
+        xhr.open(method, options.url + '?' + params, false);
+        xhr.send(null);
+    }
+}
